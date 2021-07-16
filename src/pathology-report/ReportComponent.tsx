ab228a7b18f3502b492ef28f2e4789d84078d976
bjoshuadevelopment <manshuajodevelopment@gmail.com>
2021-07-16 12:13:22 +0200

    connecting to API

--- src/pathology-report/ReportComponent.tsx
+++ src/pathology-report/ReportComponent.tsx
@@ -1,87 +1,39 @@
 import React from 'react';
 import styles from './ReportComponent.css';
 import TableRow from './TableRow';
-import getEncounters from '../Pathology-getter/Pathology-encounters-getter-resource'
+import getPatientEncounters from '../Pathology-getter/Pathology-patient-encounters-getter-resource'
+import getEncounters from '../Pathology-getter/Pathology-encounter-getter-resource'
 
-const patientList = [
-    {
-        patient_id: 22,
-        first_name: 'Richard',
-        middle_name: 'Reagan',
-        family_name: 'Ruguma',
-        phone_number: '0788446672'
-    },
-    {
-        patient_id: 15,
-        first_name: 'Michelle',
-        middle_name: '',
-        family_name: 'Nyiramwiza',
-        phone_number: '0788112233'
-    },
-    {
-        patient_id: 30,
-        first_name: 'Jelome',
-        middle_name: '',
-        family_name: 'Nteziryayo',
-        phone_number: '0788338899'
-    },
 
-]
-const pathologyRequests = [
-    {
-        encounter_id: 10,
-        patient_id: 22,
-        location_id: 20,
-        form_id: 11,
-        encounter_datetime: '2020-07-06 00:00:00',
-        creator: 22,
-        date_created: '2020-07-07 09:36:33',
-        voided: false,
-         
-    },
-    {
-        encounter_id: 11,
-        patient_id: 30,
-        location_id: 20,
-        form_id: 11,
-        encounter_datetime: '2020-07-07 00:00:00',
-        creator: 11,
-        date_created: '2020-07-08 09:36:33',
-        voided: false,
-         
-    },
-    {
-        encounter_id: 12,
-        patient_id: 15,
-        location_id: 20,
-        form_id: 11,
-        encounter_datetime: '2020-07-09 00:00:00',
-        creator: 11,
-        date_created: '2020-07-08 10:36:33',
-        voided: false,
-         
-    },
-]
 
 
 const ReportComponent = () => {
-    const [fromDate, setFromDate] = React.useState(new Date());
-    const [toDate, setToDate] = React.useState(new Date());
+    
     const [encountersList, setEncountersList] = React.useState([]);
+    // const [selectedEncounter, setSelectedEncounter] = React.useState();
+    let selectedEncounter;
 
-    const selectPatient = (patient_id) =>{
-        const patientSelected = patientList.find(pat => pat.patient_id === patient_id)
-        
-        return patientSelected;
-    }
 
     React.useEffect(() => {
-        setEncountersList(pathologyRequests);
-        getEncounters('').then( (encs) =>  {console.log(encs)});
+
+        getPatientEncounters('').then( (encs) =>  {
+            console.log(encs.data.results)
+            setEncountersList(encs.data.results);
+        });
 
     },[]);
-    // const testingDatabaseFetch = getEncounters;
-    // console.log(testingDatabaseFetch)
+
+    const encounterInfo = (encUUID) =>{
+
+        console.log(encUUID);
+        getEncounters(encUUID).then( (enc) =>  {
+            
+            console.log(enc.data.encounterDatetime)
+            return enc.data;
+            
+        })
+    }
+
     
     return (
         
@@ -119,10 +71,14 @@ const ReportComponent = () => {
                 {encountersList.map((request)=> (
                     // foo.results.find(item => item.id === 2)
                     
-
-                    <tr className={styles.tr} key={request.encounter_id}>
+                    
+                    <tr className={styles.tr} key={request.uuid}>
                         
-                        <TableRow request={request} patient={selectPatient(request.patient_id)}/>
+                        
+                        
+
+                        <TableRow request={request} encounterInfo={encounterInfo(request.uuid)}/>
+                            
                         
                     </tr>
                     
--- src/pathology-report/TableRow.tsx
+++ src/pathology-report/TableRow.tsx
@@ -3,26 +3,28 @@ import React from 'react';
 
 const TableRow = (prop) => {
     return(
+       
         <>
+            {console.log(prop.encounterInfo)}
             <td>
                 Link
             </td>
             <td>
-                {prop.patient.family_name} {' '}
+                {/* {prop.patient.family_name} {' '}
                 {prop.patient.middle_name} {' '}
-                {prop.patient.first_name} {' '}
+                {prop.patient.first_name} {' '} */}
             </td>
             <td>
                 Link
             </td>
             <td>
-                {prop.patient.phone_number}
+                {/* {prop.patient.phone_number} */}
             </td>
             <td>
                 arrived at BDH
             </td>
             <td>
-                {prop.request.encounter_datetime}
+                { prop.encounterInfo.encounterDatetime}
             </td>
             <td>
                 -
