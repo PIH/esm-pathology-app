20b11932bb23f043a637a357de4e73af21773e8e
Brandon Istenes <bistenes@gmail.com>
2022-01-14 09:39:41 -0800

    Upgrade webpack to v5; fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 
 const config = require("openmrs/default-webpack-config");
-const ProvidePlugin = require("webpack")
-config.additionalConfig.resolve={
-    fallback: {
-      process: require.resolve("process/browser"),
-      zlib: require.resolve("browserify-zlib"),
-      stream: require.resolve("stream-browserify"),
-      util: require.resolve("util"),
-      buffer: require.resolve("buffer"),
-      asset: require.resolve("assert"),
-    }
+const webpack = require("webpack");
+
+config.additionalConfig.resolve = {
+  fallback: {
+    process: require.resolve("process/browser"),
+    zlib: require.resolve("browserify-zlib"),
+    stream: require.resolve("stream-browserify"),
+    util: require.resolve("util"),
+    buffer: require.resolve("buffer"),
+    asset: require.resolve("assert"),
   }
-config.additionalConfig.plugin =  [
-  new ProvidePlugin({
+}
+config.additionalConfig.plugins = [
+  new webpack.ProvidePlugin({
     Buffer: ["buffer", "Buffer"],
     process: "process/browser",
   }),
